dc202087afe2ceb286ada89b1529b1dae19038c8
marrrunja <muammarirfan21@gmail.com>
2025-05-03 16:45:06 +0700

    g

--- database/migrations/2025_05_02_143433_create_pembelis_table.php
+++ database/migrations/2025_05_02_143433_create_pembelis_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string("username",50)->unique();
             $table->string("password",255);
+            $table->string('kode_desa');
             $table->boolean('is_active')->default(true);
             $table->timestamps();
         });
