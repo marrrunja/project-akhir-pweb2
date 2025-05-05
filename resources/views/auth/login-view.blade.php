d4f021c1cb24a780cb475edb4ee9c512218d2927
Griyo1009 <griyonugroho10@gmail.com>
2025-05-06 00:20:32 +0700

    hal login dan register

--- resources/views/auth/login-view.blade.php
+++ resources/views/auth/login-view.blade.php
@@ -1,21 +1,51 @@
 @extends('layout.layout')
-@section('title', 'Halaman Login')
+@section('title', 'Login')
 
 @push('styles')
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
 <link rel="stylesheet" href="{{ asset('resources/css/style.css') }}">  
 @endpush
+
 @section('body')
 <div class="login-container">
-    @if(Session::has('status'))
-        {{ Session::get('status') }}
-    @endif
-    <h2 style="text-align: center;">Halaman Login</h2>
-    <form method="POST" action="/login/index/post">
-        @csrf
-        <input type="text" name="username" placeholder="Username" required>
-        <input type="password" name="password" placeholder="Password" required>
-        <button role="button" type="submit">Login</button>
-        <!-- <button><a href="home.html" role="button" style="text-decoration: none;">Login</a></button> -->
-    </form>
+    <!-- Tab -->
+    <div class="d-flex justify-content-center">
+        <div style="width: 300px;">
+            <ul class="nav nav-tabs justify-content-evenly mb-3">
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ url('/register/index') }}">Sign Up</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" href="{{ url('/login-view') }}">Log In</a>
+                </li>
+            </ul>
+
+        </div>
+    </div>
+
+    <!-- Form -->
+    <div class="tab-content">
+        <div class="tab-pane fade show active">
+            <form method="POST" action="/login/index/post" class="text-center">
+                @csrf
+                @if(Session::has('status'))
+                    <div class="alert alert-danger">{{ Session::get('status') }}</div>
+                @endif
+                <div class="mb-2">
+                    <label for="username" class="text-start w-100 ps-3">Username</label>
+                    <input type="text" id="username" name="username" placeholder="Masukkan Username Anda" required>
+                </div>
+                <div class="mb-2">
+                    <label for="password" class="text-start w-100 ps-3">Password</label>
+                    <input type="password" name="password" id="password" placeholder="Masukkan Password Anda" required>
+                </div>
+                <div class="d-flex justify-content-center">
+                    <button class="w-50" type="submit">Login</button>
+                </div>
+                <p class="mt-2 text-center">Belum punya akun? <a href="/register">Register</a></p>
+            </form>
+        </div>
+    </div>
 </div>
 @endsection
--- resources/views/auth/register-view.blade.php
+++ resources/views/auth/register-view.blade.php
@@ -1,36 +1,70 @@
 @extends('layout.layout')
-@section('title', 'Halaman Register')
+@section('title', 'Register')
 
 @push('styles')
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
 <link rel="stylesheet" href="{{ asset('resources/css/style.css') }}">  
 @endpush
-
 @section('body')
-<div class="login-container">
-    @error('username')
-    {{ $message }}
-    @enderror
-    @error('desa')
-    {{ $message }}
-    @enderror
-    @error('jalan')
-    {{ $message }}
-    @enderror
-    <h2 style="text-align: center;">Halaman Register</h2>
-    <form method="POST" action="{{ route('register-proses') }}">
-        @csrf
-        <input type="text" name="username" placeholder="Username" required>
-        <input type="password" name="password" placeholder="Password" required>
-        <select name="kecamatan" id="kecamatan">
-            <option value="">Pilih Kecamatan</option>
-        </select>
-        <select name="desa" id="desa">
-            <option value="">Pilih desa</option>
-        </select>
-        <input type="text" name="alamat" placeholder="Masukkan jalan/alamat spesifik">
-        <button role="button" type="submit">Register</button>
-        <button><a href="/" role="button" style="text-decoration: none;">Kembali</a></button>
-    </form>
+    <div class="login-container">
+
+    <!-- Bagian Tab -->
+    <div class="d-flex justify-content-center">
+        <div style="width: 300px;">
+            <ul class="nav nav-tabs justify-content-evenly mb-3">
+                <li class="nav-item">
+                    <a class="nav-link active" href="{{ url('/register/index') }}">Sign Up</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ url('/login/index') }}">Log In</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+    <!-- Form -->
+    <div class="tab-content">
+        <div class="tab-pane fade show active">
+            <form method="POST" action="/register/index" class="text-center">
+                @csrf
+                @foreach (['username', 'desa', 'jalan'] as $field)
+                    @error($field)
+                        <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
+                @endforeach
+
+                <div>
+                    <label class="text-start w-100 ps-3">Username</label>
+                    <input type="text" name="username" placeholder="Masukkan Username Anda" required>
+                </div>
+                <div>
+                    <label class="text-start w-100 ps-3">Password</label>
+                    <input type="password" name="password" placeholder="Masukkan Password Anda" required>
+                </div>
+                <div>
+                    <label class="text-start w-100 ps-3">Kecamatan</label>
+                    <select name="kecamatan" id="kecamatan">
+                        <option value="">Pilih Kecamatan</option>
+                    </select>
+                </div>
+                <div>
+                    <label class="text-start w-100 ps-3">Desa</label>
+                    <select name="desa" id="desa">
+                        <option value="">Pilih Desa</option>
+                    </select>
+                </div>
+                <div>
+                    <label class="text-start w-100 ps-3">Alamat</label>
+                    <input type="text" name="alamat" placeholder="Masukkan Jalan/Alamat Spesifik">
+                </div>
+                <div class="d-flex justify-content-center">
+                    <button class="w-50" type="submit">Register</button>
+                </div>
+                <p class="mt-2 text-center">Sudah punya akun? <a href="/login">Log In</a></p>
+            </form>
+        </div>
+    </div>
 </div>
 @endsection
 
