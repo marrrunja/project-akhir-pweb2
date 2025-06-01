4a2ffadeb01708ff9f10bff673576bd258ef8791
unknown <ekaputranurdin@gmail.com>
2025-06-01 21:28:06 +0700

    c

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -177,52 +177,31 @@
                 </div>
                 <!-- /.container-fluid -->
 
-@extends('layout.layout-admin')
-@section('title', 'Halaman home admin')
+                @extends('layout.layout-admin')
+                @section('title', 'Halaman home admin')
 
 
-@push('styles')
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
-<link rel="stylesheet" type="text/css" href="{{ asset('resources/css/admin.css') }}">
-@endpush
+                @push('styles')
+                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet"
+                        integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT"
+                        crossorigin="anonymous">
+                    <link rel="stylesheet" type="text/css" href="{{ asset('resources/css/admin.css') }}">
+                @endpush
 
-@section('body')
-<section class="pt-5 pb-5">
-    <div class="container">
-        <h1>Halaman utama admin</h1>
-        <div class="row">
-            <div class="col-12 col-md-10 col-xl-5">
-                <ul class="list-group">
-                    <li class="list-group-item"> 
-                        <a href="{{ route('admin.tambah') }}" class="text-dark hover">Tambah data</a>
-                    </li>
-                    <li class="list-group-item">
-                        <a href="{{ route('admin.manage') }}" class="text-dark hover">Lihat produk</a>
-                    </li>
-                    <li class="list-group-item">
-                        <a href="{{ route('admin.order') }}" class="text-dark hover">Lihat laporan penjualan</a>
-                    </li>
-                </ul>
-            </div>
-            <!-- End of Main Content -->
+                @section('body')
 
-        </div>
-        <!-- End of Content Wrapper -->
+                <!-- End of Page Wrapper -->
 
-    </div>
-    <!-- End of Page Wrapper -->
+                <!-- Scroll to Top Button-->
+                <a class="scroll-to-top rounded" href="#page-top">
+                    <i class="fas fa-angle-up"></i>
+                </a>
 
-    <!-- Scroll to Top Button-->
-    <a class="scroll-to-top rounded" href="#page-top">
-        <i class="fas fa-angle-up"></i>
-    </a>
-
-    <!-- Scripts -->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-    <script src="js/sb-admin-2.min.js"></script>
+                <!-- Scripts -->
+                <script src="vendor/jquery/jquery.min.js"></script>
+                <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+                <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+                <script src="js/sb-admin-2.min.js"></script>
 
 </body>
 
