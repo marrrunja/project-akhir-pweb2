74a10cbde1d9b70c4e9a0f801ee335a5d7b57541
Arfun Ali Yafie <alinews120@gmail.com>
2025-06-01 18:07:13 +0700

    perbaikan login

--- resources/views/auth/login-view.blade.php
+++ resources/views/auth/login-view.blade.php
@@ -124,7 +124,7 @@
                                 @endif
                                 <div class="mb-4">
                                     <label for="username" class="form-label">Username</label>
-                                    <input type="text" class="form-control" id="username"
+                                    <input type="text" class="form-control" id="username" name="username"
                                         placeholder="Masukkan username anda" required="">
                                 </div>
 
@@ -133,7 +133,7 @@
                                         <label for="password" class="form-label">Password</label>
                                         <!-- <a href="#" class="forgot-link">Forgot password?</a> -->
                                     </div>
-                                    <input type="password" class="form-control" id="password"
+                                    <input type="password" class="form-control" id="password" name="password"
                                         placeholder="Masukkan password anda" required="">
                                 </div>
 
